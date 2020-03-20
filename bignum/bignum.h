ad91fd9374ced9bdfbe62d6ba5a9a387f7bd61ab
Steven HH Chen <s335276@gmail.com>
2020-03-21 03:04:49 +0800

    New implementation for big number storage
    Add definition of type "bignum_t" and "bnlist_t"
    Define and implement related structure operation

---
+++ bignum/bignum.h
@@ -0,0 +1,34 @@
+#ifndef BIGNUM_H
+#define BIGNUM_H
+
+typedef struct BLT {
+    char value;
+    struct BLT *prev;
+    struct BLT *next;
+} bnlist_t;
+
+typedef struct {
+    int cnt_d;
+    bnlist_t *msd;
+    bnlist_t *lsd;
+} bignum_t;
+
+/* Create a big number with initialize value zero */
+bignum_t *bn_create();
+
+/* Make fibonacci number array, store with big number structure */
+bignum_t **bn_make_fibonacci(size_t);
+
+/* Allocate digit nodes to specific input number */
+void bn_set_digit(bignum_t *, int);
+
+/* Add two big number with new memory space allocation */
+bignum_t *bn_add_with_malloc(bignum_t *, bignum_t *);
+
+/* Print big number in Big-endian(MSD first) */
+void bn_print();
+
+/* Free created space after usage */
+void bn_free();
+
+#endif
