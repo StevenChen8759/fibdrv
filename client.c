a8cf3f02c784fdf9c5276e860d003b7b50d91167
Steven HH Chen <s335276@gmail.com>
2020-03-23 06:27:59 -0700

    Add big number type testing function
    Add function fib_bignum_test() for big number testing

--- client.c
+++ client.c
@@ -5,15 +5,17 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include "bignum/bignum.h"
+
 #define FIB_DEV "/dev/fibonacci"
 
-int main()
+void fib_ull_test()
 {
     unsigned long long sz;
 
     char buf[1];
     char write_buf[] = "testing writing";
-    int offset = 100; /* TODO: try test something bigger than the limit */
+    int offset = 100;
 
     int fd = open(FIB_DEV, O_RDWR);
     if (fd < 0) {
@@ -45,5 +47,64 @@ int main()
     }
 
     close(fd);
+}
+
+/* void fib_bignum_test(void)
+{
+    unsigned long long sz;
+
+    bignum_t *fibn;
+
+    // char buf[1];
+    char write_buf[] = "testing writing";
+    int offset = 100;
+
+    int fd = open(FIB_DEV, O_RDWR);
+    if (fd < 0) {
+        perror("Failed to open character device");
+        exit(1);
+    }
+
+    for (int i = 0; i <= offset; i++) {
+        sz = write(fd, write_buf, strlen(write_buf));
+        printf("Writing to " FIB_DEV ", returned the sequence %llu\n", sz);
+    }
+
+    for (int i = 0; i <= offset; i++) {
+        lseek(fd, i, SEEK_SET);
+        sz = read(fd, fibn, 1);
+        printf("Reading from " FIB_DEV " at offset %d, returned the sequence ",
+               i);
+        bn_print(fibn);
+        printf(".\n");
+    }
+
+    for (int i = offset; i >= 0; i--) {
+        lseek(fd, i, SEEK_SET);
+        // sz = read(fd, fibn, 1);
+        read(fd, fibn, 1);
+
+        printf("Position:");
+
+        printf("Reading from " FIB_DEV " at offset %d, returned the sequence ",
+               i);
+        bn_print(fibn);
+        printf(".\n");
+    }
+
+    close(fd);
+} */
+
+int main()
+{
+    /* Call different function for specific implementaion */
+
+    fib_ull_test();  // In type unsigned long long
+    // fib_bignum_test(); // In type bignum_t
+
+    /* Using fast doubling implementation */
+    // fib_ull_fd_test();    // In type unsigned long long
+    // fib_bignum_fd_test(); // In type bignum_t
+
     return 0;
 }
